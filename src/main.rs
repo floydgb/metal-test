6b685fc8515cf4396068a4a1b13b92bef6e75fa7
greg floyd <greg.floyd@showerstream.net>
2023-09-04 03:55:45 -0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ pub struct GPU {
 // Public Functions -----------------------------------------------------------
 pub fn main() {
     let size = 1_000_000_000;
-    let gpu = gpu_setup();
+    let gpu = setup();
     loop {
         let rand_start = std::time::Instant::now();
         let a: Vec<f32> = (0..size)
@@ -58,43 +58,43 @@ pub fn cpu_dot(a: &[f32], b: &[f32]) -> f32 {
 pub fn gpu_dot(a: &[f32], b: &[f32], gpu: &GPU) -> f32 {
     let command_buffer = gpu.cmds.new_command_buffer();
     let encoder = command_buffer.new_compute_command_encoder();
-    let buf_result = gpu_set_bufs(Arc::clone(&gpu.dev), encoder, a, b);
+    let buf_result = set_bufs(Arc::clone(&gpu.dev), encoder, a, b);
     encoder.set_compute_pipeline_state(&gpu.state);
-    gpu_run(command_buffer, encoder, a.len());
-    gpu_result(buf_result, a.len())
+    run_func(command_buffer, encoder, a.len());
+    result(buf_result, a.len())
 }
 
-pub fn gpu_setup() -> GPU {
+pub fn setup() -> GPU {
     let dev = Arc::new(Device::system_default().expect("Apple Metal GPU"));
     let cmds = Arc::new(dev.new_command_queue());
-    let state = Arc::new(gpu_state(Arc::clone(&dev)).unwrap());
+    let state = Arc::new(state(Arc::clone(&dev)).unwrap());
     GPU { dev, cmds, state }
 }
 
-pub fn gpu_result(buf_result: Buffer, len: usize) -> f32 {
+pub fn result(buf_result: Buffer, len: usize) -> f32 {
     unsafe { std::slice::from_raw_parts(buf_result.contents() as *const _, len) }
         .iter()
         .sum()
 }
 
-pub fn gpu_run(command_buffer: &CommandBufferRef, encoder: &ComputeCommandEncoderRef, len: usize) {
+pub fn run_func(command_buffer: &CommandBufferRef, encoder: &ComputeCommandEncoderRef, len: usize) {
     let threads_per_threadgroup: u64 = 64;
     let threadgroups = (len as f64 / threads_per_threadgroup as f64).ceil() as u64;
-    encoder.dispatch_thread_groups(gpu_1d_size(threadgroups), gpu_1d_size(threads_per_threadgroup));
+    encoder.dispatch_thread_groups(size_1d(threadgroups), size_1d(threads_per_threadgroup));
     encoder.end_encoding();
     command_buffer.commit();
     command_buffer.wait_until_completed();
 }
 
-pub fn gpu_set_bufs(dev: Arc<Device>, enc: &ComputeCommandEncoderRef, a: &[f32], b: &[f32]) -> Buffer {
-    let buf_result = gpu_buf(&dev, &vec![0.0; a.len()]);
-    enc.set_buffer(0, Some(&gpu_buf(&dev, a)), 0);
-    enc.set_buffer(1, Some(&gpu_buf(&dev, b)), 0);
+pub fn set_bufs(dev: Arc<Device>, enc: &ComputeCommandEncoderRef, a: &[f32], b: &[f32]) -> Buffer {
+    let buf_result = buf(&dev, &vec![0.0; a.len()]);
+    enc.set_buffer(0, Some(&buf(&dev, a)), 0);
+    enc.set_buffer(1, Some(&buf(&dev, b)), 0);
     enc.set_buffer(2, Some(&buf_result), 0);
     buf_result
 }
 
-pub fn gpu_1d_size(width: u64) -> MTLSize {
+pub fn size_1d(width: u64) -> MTLSize {
     let (height, depth) = (1, 1);
     MTLSize {
         width,
@@ -103,7 +103,7 @@ pub fn gpu_1d_size(width: u64) -> MTLSize {
     }
 }
 
-pub fn gpu_buf(dev: &Arc<Device>, buf: &[f32]) -> Buffer {
+pub fn buf(dev: &Arc<Device>, buf: &[f32]) -> Buffer {
     dev.new_buffer_with_data(
         buf.as_ptr() as *const std::ffi::c_void,
         (buf.len() * std::mem::size_of::<f32>()) as u64,
@@ -111,7 +111,7 @@ pub fn gpu_buf(dev: &Arc<Device>, buf: &[f32]) -> Buffer {
     )
 }
 
-pub fn gpu_fn(device: &Arc<Device>, fn_name: String) -> Result<Function, String> {
+pub fn func(device: &Arc<Device>, fn_name: String) -> Result<Function, String> {
     let source = include_str!("shader.metal");
     let options = metal::CompileOptions::new();
     let library = device
@@ -120,8 +120,8 @@ pub fn gpu_fn(device: &Arc<Device>, fn_name: String) -> Result<Function, String>
     library.get_function(&fn_name, None)
 }
 
-pub fn gpu_state(device: Arc<Device>) -> Result<ComputePipelineState, String> {
-    let dot_fn = gpu_fn(&device, "dot_product".to_string()).unwrap();
+pub fn state(device: Arc<Device>) -> Result<ComputePipelineState, String> {
+    let dot_fn = func(&device, "dot_product".to_string()).unwrap();
     let pipeline_desc = ComputePipelineDescriptor::new();
     pipeline_desc.set_compute_function(Some(&dot_fn));
     device.new_compute_pipeline_state(&pipeline_desc)
